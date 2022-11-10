cbd702809383d0a6042a3038192f15cc3ca2f9ec
Trevor Keller <trevor.keller@nist.gov>
2022-11-10 17:24:27 -0500

    tweaks with expert advice

--- fipy-1a-orig.py
+++ fipy-1a-orig.py
@@ -1,30 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
+#!/usr/bin/env python3
 
 # # PFHub BM 1a in FiPy with Steppyngstounes
-# 
+#
 # This notebook implements [PFHub] Benchmark [1a][spinodal] using [FiPy] and [steppyngstounes].
 # It also explores alternative initial conditions that are more-periodic than the specification.
-# 
+#
 # <!-- links -->
 # [FiPy]: https://www.ctcms.nist.gov/fipy
 # [spinodal]: https://pages.nist.gov/pfhub/benchmarks/benchmark1.ipynb/#(a)-Square-periodic
 # [steppyngstounes]: https://github.com/usnistgov/steppyngstounes
 # [PFHub]: https://pages.nist.gov/pfhub/
 
-# In[ ]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
 import gc
-import ipywidgets as widgets
+import matplotlib
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import psutil
 import sympy.abc
 
-from IPython.display import display, update_display
 from sympy import Eq, diff, expand, factor, symbols
 from tqdm import tqdm
 import time
@@ -33,18 +27,19 @@ from steppyngstounes import CheckpointStepper, PIDStepper
 
 from fipy import Variable, FaceVariable, CellVariable
 from fipy import DiffusionTerm, ImplicitSourceTerm, TransientTerm
-from fipy import numerix, parallel, Viewer
+from fipy import numerix, parallel
+from fipy import Matplotlib2DViewer as Viewer
 
 from fipy import PeriodicGrid2D as Grid2D
-from fipy.solvers.petsc import LinearLUSolver as Solver
+from fipy.solvers.pyamgx import LinearLUSolver as Solver
+
+matplotlib.use("agg")
+
+isHead = parallel.procID == 0
 
 cos = numerix.cos
 pi = numerix.pi
 proc = psutil.Process()
-
-
-# In[ ]:
-
 
 try:
     startTime = time.time_ns()
@@ -53,11 +48,7 @@ except AttributeError:
     startTime = time.time()
     time_has_ns = False
 
-
 # ## Prepare mesh & phase field
-
-# In[ ]:
-
 
 nx = ny = 200
 dx = dy = 1
@@ -65,67 +56,37 @@ Lx = nx * dx
 Ly = ny * dy
 mesh = Grid2D(nx=nx, ny=ny, dx=dx, dy=dy)
 
-
-# In[ ]:
-
-
 x, y = mesh.cellCenters
 c = CellVariable(mesh=mesh, name=r"$c$",   value=0.0, hasOld=True)
 μ = CellVariable(mesh=mesh, name=r"$\mu$", value=0.5, hasOld=True)
 
 
 # ## Define equations of motion
-# 
-# This is based on [fipy.examples.cahnHilliard.mesh2DCoupled](https://www.ctcms.nist.gov/fipy/examples/cahnHilliard/generated/examples.cahnHilliard.mesh2DCoupled.html). The coupled equations are
-# 
+#
+# This is based on
+# https://www.ctcms.nist.gov/fipy/examples/cahnHilliard/generated/examples.cahnHilliard.mesh2DCoupled.html
+# The coupled equations are
+#
 # $$
 # \frac{\partial c}{\partial t} = \nabla \cdot \nabla\mu
 # $$
-# 
+#
 # $$
 # \mu = \left(\frac{\partial f}{\partial c} - \frac{\partial^2 f}{\partial c^2} \cdot c\right)_{\mathrm{old}} + \frac{\partial^2 f}{\partial c^2}\cdot c - \kappa \nabla^2 c
 # $$
-# 
+#
 # where the second term on $\mu$ is an `ImplicitSourceTerm` and the last is a `DiffusionTerm`.
-
-# In[ ]:
-
-
-fchem = sympy.abc.rho * (sympy.abc.c - sympy.abc.alpha)**2 \
-                      * (sympy.abc.beta - sympy.abc.c)**2
-
-display(Eq(symbols("f"), fchem))
-display(Eq(symbols("f'"), factor(diff(fchem,
-                                     sympy.abc.c))))
-display(Eq(symbols("f''"), factor(expand(diff(fchem,
-                                             sympy.abc.c,
-                                             sympy.abc.c)))))
-
-
-# $$f_{\mathrm{c}} = \rho (c - \alpha)^2 (\beta - c)^2$$
-# 
-# $$f'_{\mathrm{c}} = 2\rho (c - \alpha)(\beta - c)(\alpha - 2c + \beta)$$
-# 
-# $$f''_{\mathrm{c}} = 2\rho\left\{\alpha^2 + 4 \alpha \beta + \beta^2 - 6 c \left(\alpha - c + \beta\right)\right\}$$
-
-# In[ ]:
-
 
 α = 0.3
 β = 0.7
 ρ = 5
+κ = 2
+M = 5
 
 fc = ρ * (c - α)**2 * (β - c)**2
 dfdc = 2 * ρ * (c - α) * (β - c) * (α - 2 * c + β)
 d2fc = 2 * ρ * (α**2 + 4*α*β + β**2 \
                   - 6 * c * (α - c + β))
-
-
-# In[ ]:
-
-
-κ = 2
-M = 5
 
 eom_c = TransientTerm(var=c) == DiffusionTerm(coeff=M, var=μ)
 eom_μ = ImplicitSourceTerm(coeff=1.0, var=μ) \
@@ -135,11 +96,7 @@ eom_μ = ImplicitSourceTerm(coeff=1.0, var=μ) \
 
 eom = eom_c & eom_μ
 
-
 # ## Initial Conditions
-
-# In[ ]:
-
 
 t = Variable(0)
 dt = Variable(1e-5)
@@ -154,10 +111,6 @@ def initialize(A, B):
         +  cos(A[2] * x  +     B[2] * y) \
         *  cos(A[3] * x  +     B[3] * y)
     )
-
-
-# In[ ]:
-
 
 # BM 1a specification: not periodic at all
 
@@ -174,23 +127,21 @@ iodir = "1a.0"
 if not os.path.exists(iodir):
     os.mkdir(iodir)
 
-
-# In[ ]:
-
-
-viewer = Viewer(vars=(c,), datamin=0., datamax=1., cmap="coolwarm")
-
 def write_img():
-    viewer.title = r"$t=%g$" % t
-    viewer.plot("{iodir}/comp_{t:0>12.0f}.png")
-    
+    # opt. 1: dump the numpy array; it's linear
+    #         (shape=(n*m,1), want (n,m))
+    #         call var.globalValue.reshape(n,m)
+    #         ... then visualize in matplotlib later
+    # opt. 2: have each process write its PNG to its own rank-labeled
+    #         file, then stitch together
+    # opt. 3: PyAMGX
+    if isHead:
+        viewer.title = r"$t=%g$" % t
+        viewer.plot("{}/comp_{}.png".format(iodir, str(int(t)).zfill(7)))
+
 write_img()
 
-
 # ## Prepare free energy output
-
-# In[ ]:
-
 
 df = None
 
@@ -202,28 +153,27 @@ def update_energy():
         timer = 1e-9 * (time.time_ns() - startTime)
     else:
         timer = time.time() - startTime
-    if parallel.procID == 0:
+    if isHead:
         labs = ["wall_time", "time", "free_energy",
                 "mem_MB", "timestep", "mass"]
         vals = [[timer, t, nrg, mem, dt, mas]]
         indx = [0] if df is None \
                    else [len(df)]
-        
+
         return pd.concat([df,
                           pd.DataFrame(vals,
                                        columns=labs,
                                        index=indx)]
                         )
-   
+
+    return None
+
 df = update_energy()
 
-# disp_nrg = display(df.tail(), display_id="nrg")
-
+if isHead:
+    df.to_csv("{iodir}/energy.csv.gz")
 
 # ## Timestepping
-
-# In[ ]:
-
 
 fin = 2e6
 rtol = 1e-3
@@ -234,19 +184,15 @@ chkpts = [p * 10**q \
           for q in range(6) \
           for p in (1, 2, 5)]
 
-
-# In[ ]:
-
-
 for check in CheckpointStepper(start=0.0,
-                               stops=chkpts
+                               stops=chkpts,
                                stop=fin):
     for step in PIDStepper(start=check.begin,
-                           stop=check.end, 
+                           stop=check.end,
                            size=dt):
         for sweep in range(2):
             res = eom.sweep(dt=step.size, solver=solver)
-    
+
         if step.succeeded(error=res/rtol):
             t.value = step.end
             dt = step.size
@@ -258,19 +204,9 @@ for check in CheckpointStepper(start=0.0,
             μ.value = μ.old
 
         gc.collect()
-        
+
     if check.succeeded():
         dt = step.want
-        df.to_csv("{iodir}/energy.csv.gz")
+        if isHead:
+            df.to_csv("{}/energy.csv.gz".format(iodir))
         write_img()
-        # update_display(df.tail(), display_id="nrg")
-        # update_display(viewer.plot(), display_id="plt")
-        # viewer.title = r"$t=%g$" % t
-        # viewer.plot()
-
-
-# In[ ]:
-
-
-
-
