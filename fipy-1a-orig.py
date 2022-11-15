00d42f787170d1c05e6c86b2dafa993a6c18c1d8
Trevor Keller <trevor.keller@nist.gov>
2022-11-15 09:52:52 -0500

    functional non-periodic line in the sand

--- fipy-1a-orig.py
+++ fipy-1a-orig.py
@@ -2,37 +2,32 @@
 # coding: utf-8
 
 # # PFHub BM 1a in FiPy with Steppyngstounes
-# 
+#
 # This notebook implements [PFHub] Benchmark [1a][spinodal] using [FiPy] and [steppyngstounes].
 # It also explores alternative initial conditions that are more-periodic than the specification.
-# 
+#
 # <!-- links -->
 # [FiPy]: https://www.ctcms.nist.gov/fipy
 # [spinodal]: https://pages.nist.gov/pfhub/benchmarks/benchmark1.ipynb/#(a)-Square-periodic
 # [steppyngstounes]: https://github.com/usnistgov/steppyngstounes
 # [PFHub]: https://pages.nist.gov/pfhub/
 
-# In[ ]:
-
+# from memory_profiler import profile
 
 import gc
 import os
 import psutil
 import time
 
-from fipy import Variable, FaceVariable, CellVariable
+from fipy import Variable, CellVariable
 from fipy import DiffusionTerm, ImplicitSourceTerm, TransientTerm
-from fipy import numerix, parallel, Viewer
+from fipy import numerix, parallel
 
-from fipy import PeriodicGrid2D as Grid2D
+from fipy import Grid2D
 from fipy.solvers.petsc import LinearLUSolver as Solver
 from fipy.solvers.petsc.comms import petscCommWrapper
 
 from steppyngstounes import CheckpointStepper, PIDStepper
-
-
-# In[ ]:
-
 
 try:
     startTime = time.time_ns()
@@ -46,30 +41,17 @@ pi = numerix.pi
 proc = psutil.Process()
 
 comm = petscCommWrapper.PETScCommWrapper()
-procID = parallel.procID
-
+rank = parallel.procID
 
 # ## Prepare mesh & phase field
 
-# In[ ]:
-
-
-nx = ny = 200
-dx = dy = 1
-Lx = nx * dx
-Ly = ny * dy
-
-mesh = Grid2D(nx=nx, ny=ny, dx=dx, dy=dy)
+mesh = Grid2D(nx=200, ny=200, dx=0.375, dy=0.375)
 x, y = mesh.cellCenters
 
-c = CellVariable(mesh=mesh, name=r"$c$",   value=0.0, hasOld=True)
-μ = CellVariable(mesh=mesh, name=r"$\mu$", value=0.5, hasOld=True)
-
+c = CellVariable(mesh=mesh, name=r"$c$",   hasOld=True)
+μ = CellVariable(mesh=mesh, name=r"$\mu$", hasOld=True)
 
 # ### Set thermo-kinetic constants from the BM1 specification
-
-# In[ ]:
-
 
 α = 0.3
 β = 0.7
@@ -77,27 +59,23 @@ c = CellVariable(mesh=mesh, name=r"$c$",   value=0.0, hasOld=True)
 κ = 2
 M = 5
 
-
 # ## Define equations of motion
-# 
+#
 # This is based on [fipy.examples.cahnHilliard.mesh2DCoupled],
 # using a first-order Taylor series substitution in place of the bulk free energy "source term".
 # The coupled equations are
-# 
+#
 # $$
 # \frac{\partial c}{\partial t} = \nabla \cdot M\nabla\mu
 # $$
-# 
+#
 # $$
 # \mu = \left(\frac{\partial f_{\mathrm{bulk}}}{\partial c} - \frac{\partial^2 f_{\mathrm{bulk}}}{\partial c^2} \cdot c\right)_{\mathrm{old}} + \frac{\partial^2 f_{\mathrm{bulk}}}{\partial c^2}\cdot c - \kappa \nabla^2 c
 # $$
-# 
+#
 # where the second term on $\mu$ is an `ImplicitSourceTerm` and the last is a `DiffusionTerm`.
-# 
+#
 # [fipy.examples.cahnHilliard.mesh2DCoupled]: https://www.ctcms.nist.gov/fipy/examples/cahnHilliard/generated/examples.cahnHilliard.mesh2DCoupled.html
-
-# In[ ]:
-
 
 """
 ## Uncomment and run this cell to double-check the derivatives.
@@ -115,25 +93,17 @@ display(Eq(symbols("f''"), factor(expand(diff(fbulk,
                                              sympy.abc.c)))))
 """
 
-
 # The free energy density and its first two derivatives are (refactored after SymPy)
-# 
+#
 # $$f_{\mathrm{bulk}} = \rho (c - \alpha)^2 (\beta - c)^2$$
-# 
+#
 # $$f'_{\mathrm{bulk}} = 2\rho (c - \alpha)(\beta - c)(\alpha - 2c + \beta)$$
-# 
+#
 # $$f''_{\mathrm{bulk}} = 2\rho\left\{\alpha^2 + 4 \alpha \beta + \beta^2 - 6 c \left(\alpha - c + \beta\right)\right\}$$
-
-# In[ ]:
-
 
 fbulk = ρ * (c - α)**2 * (β - c)**2
 d1fdc = 2 * ρ * (c - α) * (β - c) * (α - 2 * c + β)
 d2fdc = 2 * ρ * (α**2 + 4*α*β + β**2 - 6 * c * (α - c + β))
-
-
-# In[ ]:
-
 
 eom_c = TransientTerm(var=c) == DiffusionTerm(coeff=M, var=μ)
 
@@ -144,26 +114,19 @@ eom_μ = ImplicitSourceTerm(coeff=1.0, var=μ) \
 
 eom = eom_c & eom_μ
 
-
 # ## Initial Conditions -- As Specified
 
-# In[ ]:
+if rank == 0:
+    iodir = "1a.0"
 
-
-iodir = "1a.0"
-
-if not os.path.exists(iodir):
-    os.mkdir(iodir)
-
-
-# In[ ]:
-
+    if not os.path.exists(iodir):
+        os.mkdir(iodir)
 
 t = Variable(0.0)
 dt = Variable(1e-5)
 
 c0 = 0.5
-ϵ = 0.01
+ϵ  = 0.01
 
 def initialize(A, B):
     return c0 + ϵ * (
@@ -172,9 +135,6 @@ def initialize(A, B):
         +  cos(A[2] * x  +     B[2] * y) \
         *  cos(A[3] * x  +     B[3] * y)
     )
-
-
-# In[ ]:
 
 
 # BM 1a specification: not periodic at all
@@ -187,24 +147,18 @@ c.value = initialize(A0, B0)
 c.updateOld()
 μ.updateOld()
 
-
 # ## Prepare free energy output
 
-# In[ ]:
-
-
-df = None
-
 labs = [
-    "wall_time", 
-    "time",     
+    "wall_time",
+    "time",
     "free_energy",
-    "mem_GB",    
-    "timestep", 
+    "mem_GB",
+    "timestep",
     "mass"
 ]
 
-if procID == 0:  # write the CSV header
+if rank == 0:  # write the CSV header
     fcsv = "{}/energy.csv".format(iodir)
     with open(fcsv, "w") as fh:
         fh.write("{},{},{},{},{},{}\n".format(*labs))
@@ -212,79 +166,72 @@ else:
     fcsv = None
 
 def update_energy(fh=None):
-    # Integration of fields: CellVolumeAverage, .sum(), 
+    # Integration of fields: CellVolumeAverage, .sum(),
     nrg = (fbulk - 0.5 * κ * numerix.dot(c.grad, c.grad)).sum()
     mas = c.sum()
     mem = comm.allgather(proc.memory_info().rss) / 1024**3
-    if procID == 0:
+    if rank == 0:
         if time_has_ns:
             timer = 1e-9 * (time.time_ns() - startTime)
         else:
             timer = time.time() - startTime
-    
+
         vals = [timer, t, nrg, mem, dt, mas]
-        
+
         with open(fcsv, "a") as fh:
             fh.write("{},{},{},{},{},{}\n".format(*vals))
-        
+
 update_energy(fcsv)
 
 
 # ## Timestepping
 
-# In[ ]:
-
-
-fin = 2e6
 rtol = 1e-3
 solver = Solver()
 
 # Write to disk every 1, 2, 5, 10, 20, 50, ...
 chkpts = [p * 10**q \
-          for q in range(3) \
+          for q in (-1, 0) \
           for p in (1, 2, 5)]
 
 print("Writing a checkpoint at the following times:")
 print(chkpts)
 
+# @profile
 
-# In[ ]:
+def run():
+    global dt
+    for check in CheckpointStepper(start=0.0,
+                                   stops=chkpts,
+                                   stop=chkpts[-1]):
+        if rank == 0:
+            print("Launching [{:12g} .. {:12g})".format(check.begin, check.end))
 
+        for step in PIDStepper(start=check.begin,
+                               stop=check.end,
+                               size=dt):
+            if rank == 0:
+                print("  Stepping [{:12g} .. {:12g}) / {:12g}".format(float(step.begin),
+                                                                            float(step.end),
+                                                                            float(step.size)))
 
-for check in CheckpointStepper(start=0.0,
-                               stops=chkpts,
-                               stop=fin):
-    for step in PIDStepper(start=check.begin,
-                           stop=check.end, 
-                           size=dt):
-        for sweep in range(2):
-            res = eom.sweep(dt=step.size, solver=solver)
-    
-        if step.succeeded(error=res/rtol):
-            t.value = step.end
-            dt = step.size
-            c.updateOld()
-            μ.updateOld()
-            # df = update_energy()
-            update_energy(fcsv)
-        else:
-            c.value = c.old
-            μ.value = μ.old
+            for sweep in range(2):
+                res = eom.sweep(dt=step.size, solver=solver)
 
-        gc.collect()
-        
-    if check.succeeded():
-        dt = step.want
-        # df.to_csv("{}/energy.csv.gz".format(iodir))
-        # write_img()
-        # update_display(df.tail(), display_id="nrg")
-        # update_display(viewer.plot(), display_id="plt")
-        # viewer.title = r"$t=%g$" % t
-        # viewer.plot()
+            if step.succeeded(error=res/rtol):
+                dt.value = step.size
+                t.value += dt
+                c.updateOld()
+                μ.updateOld()
+                update_energy(fcsv)
+            else:
+                print("        Failed.")
+                c.value = c.old
+                μ.value = μ.old
 
+            gc.collect()
 
-# In[ ]:
+        if check.succeeded():
+            dt.value = step.want
 
-
-
-
+run()
