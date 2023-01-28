866fd47c17ba65b7ef3b03149bcff79dc6ebe3b8
Trevor Keller <trevor.keller@nist.gov>
2023-01-28 11:38:13 -0500

    plot FiPy, PyMKS, and residuals, too!

--- plot_energy.py
+++ plot_energy.py
@@ -12,60 +12,102 @@ from os import path
 
 from argparse import ArgumentParser
 
-variants = ("orig", "peri", "zany")
+variants = {
+    "orig": "original",
+    "peri": "periodic",
+    "zany": "timbrous"
+}
+
 figsize = (6, 5)
 dpi=400
 
 parser = ArgumentParser(
-    prog = 'fipy-bm1-variations',
+    prog = 'plot_energy',
     description = 'Plot results of PFHub BM1 in FiPy with periodic initial condition variations'
 )
 
+parser.add_argument("-p", "--prefix",
+                    default=".",
+                    help="prefix (directory) for variants")
+parser.add_argument("-s", "--software",
+                    default="FiPy",
+                    help="software platform")
+
 args = parser.parse_args()
 
-def read_and_plot(iodir):
-    csv_file = "{}/energy.csv".format(iodir)
-    df = None
-    if path.exists(csv_file):
-        df = pandas.read_csv("{}/energy.csv".format(iodir))
-        df.time[0] = 0.875 * df.time[1]
+def read_and_plot(prefix, variant, software=None):
+    iodir = prefix + "/" + variant
+    data_file = "{}/energy.csv".format(iodir)
+    if not path.exists(data_file):
+        data_file = "{}/free_energy.csv.gz".format(iodir)
+    nrg_image = "{}/energy.png".format(iodir)
+    mem_image = "{}/memory.png".format(iodir)
+
+    if path.exists(data_file):
+        df = pandas.read_csv(data_file)
+        df.time[0] = 0.95 * df.time[1]  # no zeros in log-log plot
+        mem_col = [c for c in df.columns.values if c.startswith("mem")]
+        mem_scl = mem_col[0].replace("mem_", "")
 
         plt.figure(1, figsize=figsize)
+        if software is not None:
+            plt.title("BM 1a: {}".format(software))
         plt.xlabel(r"time $t$ / [a.u.]")
         plt.ylabel(r"Free energy $\mathcal{F}$ / [J/m³]")
-        plt.loglog(df.time, df.free_energy, label=iodir)
-        plt.savefig("{}/energy.png".format(iodir), bbox_inches="tight", dpi=dpi)
+        plt.loglog(df.time, df.free_energy, label=variants[variant])
+        plt.legend(loc="best")
+        plt.savefig(nrg_image, bbox_inches="tight", dpi=dpi)
         plt.close()
 
         plt.figure(2, figsize=figsize)
+        if software is not None:
+            plt.title("BM 1a: {}".format(software))
         plt.xlabel(r"time $t$ / [a.u.]")
-        plt.ylabel("Memory / [MB]")
-        plt.loglog(df.time, df.mem_MB)
-        plt.savefig("{}/memory.png".format(iodir), bbox_inches="tight", dpi=dpi)
+        plt.ylabel("Memory / [{}]".format(mem_scl))
+        plt.loglog(df.time, df[mem_col], label=variants[variant])
+        plt.legend(loc="best")
+        plt.savefig(mem_image, bbox_inches="tight", dpi=dpi)
         plt.close()
 
-    return df
+        if plt.fignum_exists(3):
+            plt.figure(3)
+            plt.loglog(df.time, df.free_energy, label=variants[variant])
 
 
-def plot_all(dirs):
-    dframes = {}
+def residual_plot(prefix, variant, software=None):
+    iodir = prefix + "/" + variant
+    data_file = "{}/residual.csv.gz".format(iodir)
+    res_image = "{}/residual.png".format(iodir)
+    if path.exists(data_file):
+        df = pandas.read_csv(data_file)
+        plt.figure(1, figsize=figsize)
+        if software is not None:
+            plt.title("BM 1a: {}".format(software))
+        plt.xlabel(r"time $t$ / [a.u.]")
+        plt.ylabel(r"Residual")
+        plt.scatter(df.time * (100 + df.sweep)/100, df.residual, alpha=0.5, label=variants[variant])
+        plt.yscale("log")
+        plt.legend(loc="best")
+        plt.savefig(res_image, bbox_inches="tight", dpi=dpi)
+        plt.close()
 
-    # create energy & memory plots for each initial condition
-    for dirname in dirs:
-        if path.exists(dirname):
-            dframes[dirname] = read_and_plot(dirname)
 
-    # co-plot all energy results
+def plot_all(prefix=".", software="FiPy"):
+    # create energy & memory plots for each variant,
+    # and co-plot all energy results
     plt.figure(3, figsize=figsize)
+    plt.title("BM 1a: {}".format(software))
     plt.xlabel(r"time $t$ / [a.u.]")
     plt.ylabel(r"Free energy $\mathcal{F}$ / [J/m³]")
-    for dirname, df in dframes.items():
-        if df is not None:
-            plt.loglog(df.time, df.free_energy, label=dirname)
+    for variant in variants.keys():
+        read_and_plot(prefix, variant, software)
+        residual_plot(prefix, variant, software)
+    plt.figure(3)
     plt.legend(loc="best")
-    plt.savefig("energies.png", bbox_inches="tight", dpi=dpi)
+    nrg_image = "{}/energies.png".format(prefix)
+    plt.savefig(nrg_image, bbox_inches="tight", dpi=dpi)
     plt.close()
 
 if __name__ == "__main__":
     # read_and_plot(iodir)
-    plot_all(variants)
+    plot_all(args.prefix, args.software)
